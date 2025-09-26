9edcd8b05ba0583b50d2140a63883db4c2da6e68
raunakdeveloper <raunakkaushal.rk@gmail.com>
2025-09-26 05:56:26 +0530

    change carousel image

--- frontend/src/components/HeroCarousel.jsx
+++ frontend/src/components/HeroCarousel.jsx
@@ -4,7 +4,7 @@ import img1 from "../assets/carousel/img1.png";
 import img2 from "../assets/carousel/img2.png";
 import img3 from "../assets/carousel/img3.png";
 import img4 from "../assets/carousel/img4.png";
-import img5 from "../assets/carousel/img5.jpg";
+import img5 from "../assets/carousel/img5.png";
 
 const HeroCarousel = () => {
   const images = [img1, img2, img3, img4, img5];
